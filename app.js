9e5e7606700b7a02b06f66021d7d68393a50307f
Jan <pcerservices@gmail.com>
2023-07-28 18:20:17 +0200

    Added Mongoose, MongoDB connection and Cors

--- app.js
+++ app.js
@@ -1,15 +1,27 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 require('dotenv').config({path: __dirname + '/.env'});
+const mongoose = require('mongoose');
+const cors = require('cors');
 
 const app = express();
 
+app.set('view engine', 'ejs');
+
 app.use(bodyParser.urlencoded({extended: true}));
 
-const URI = process.env.API_TEST;
+app.use(cors());
+
+const URI = process.env.ATLAS_URI;
 
 app.listen(3000, function() {
     console.log("Listening on port 3000");
+})
+
+mongoose.connect(URI);
+const connection = mongoose.connection;
+connection.once('open', () => {
+    console.log('MongoDB database connection established successfully');
 })
 
 app.get("/", function(req, res) {
