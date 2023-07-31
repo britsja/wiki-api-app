37535b3313f95669fa2a0dbb674df30cd5a24d69
Jan <pcerservices@gmail.com>
2023-07-31 12:19:25 +0200

    Added method to get, create and delete entries through API

--- app.js
+++ app.js
@@ -12,6 +12,8 @@ app.use(bodyParser.urlencoded({extended: true}));
 
 app.use(cors());
 
+app.use(express.static("public"));
+
 const URI = process.env.ATLAS_URI;
 
 app.listen(3000, function() {
@@ -24,6 +26,53 @@ connection.once('open', () => {
     console.log('MongoDB database connection established successfully');
 })
 
+const articleSchema = {
+    title: String,
+    content: String
+};
+
+const Article = mongoose.model("Article", articleSchema);
+
 app.get("/", function(req, res) {
-    res.send(`<h1>Wiki API</h1><p>${URI}</p>`);
+    res.send("Use /articles to get API data")    
 })
+
+app.get("/articles", function(req, res) {
+    try {
+    Article.find().then((data) => {
+        if (data.length > 0) {
+            const returnedArticles = data;
+            res.send(data);
+        } else {
+            res.send("Database is empty");
+        }
+    });
+    } catch {
+        res.send("Error getting data from database");
+    }
+    
+});
+
+app.post("/articles", async function(req, res) {
+    try {
+        const newArticle = new Article({
+            title: req.body.title,
+            content: req.body.content
+        });
+        await newArticle.save();
+        res.send("Article Submitted");
+    } catch {
+        res.send("Cannot Save Article - Please ensure you are using title and content for the form names")
+    }
+});
+
+app.delete("/articles", async function(req, res) {
+    try {
+        await Article.deleteMany();
+        res.send("Articles Deleted")
+    } 
+    catch {
+        res.send("Error deleting all articles")
+    }   
+});
+
